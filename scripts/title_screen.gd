a5f4ffb51bf2097c01d069955221f039184ad3d5
Zander Davidson <redna@MacBook-Pro-2.local>
2025-10-26 16:59:34 -0400

    instructions on title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -4,5 +4,11 @@ func _ready() -> void:
 	match Settings.get_setting("controls", "control_mode"):
 		Enum.ControlMode.MOUSE_AND_KEYBOARD:
 			$PressStartLabel.text = "press space to start"
+			$MoveAndAimLabel.text = "WASD - move"
+			$ShootAndSwitchLabel.text = "left click - fire weapon\nright click - switch weapon"
+			
 		Enum.ControlMode.GAMEPAD:
 			$PressStartLabel.text = "press A to start"
+			$MoveAndAimLabel.text = "left stick - move\nright stick - aim"
+			$ShootAndSwitchLabel.text = "right trigger - fire weapon\nleft trigger - switch weapon"
+			
